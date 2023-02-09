af8f1ad41647fbf5db94b0a26074380f094211cb
Lucas David <lucascanellasdev@gmail.com>
2023-02-09 15:41:12 -0300

    deploy postgres

--- src/main/java/com/lucasdc/shoppingifyapi/domain/models/User.java
+++ src/main/java/com/lucasdc/shoppingifyapi/domain/models/User.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +23,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "tb_user")
 public class User implements UserDetails {
     
     @Id
